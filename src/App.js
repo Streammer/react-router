ed4ec2a7583bf557dbe273ad4579d063477b7cfa
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-11-01 11:13:41 +0300

    About, Home and Profile pages was been created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
 
 function App() {
-  return (
-    <div className="App">
-     <Header />
-    </div>
-  );
+	return (
+		<BrowserRouter 
+		// forceRefresh
+		// basename='react-router-tutorial' 
+		// getUserConfirmation ={
+		// 	(message, callback)=> {
+		// 		callback(window.confirm())
+		// 	}
+		// }
+		>
+			<div className="App">
+				<Header />
+			</div>
+
+			<Switch>
+				<Route path='/' component={Home} exact />
+				<Route path='/about' component={About} />
+				<Route path='/profile' component={Profile} />
+			</Switch>
+
+		</BrowserRouter>
+
+	);
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <>
             <h1>React Router Dom Tutorial</h1>
-        </div>
+
+            <ul className='nav'>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/profile'>Profile</Link>
+                </li>
+            </ul>
+        </>
     );
 };
 

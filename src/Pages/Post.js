a839d14f008036a941622bb3e8c85cac7c064e18
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-11-01 12:17:15 +0300

    UseLocation hook

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 
-const Post = ({match}) => {
+const Post = ({ match }) => {
 
-    const {id} = useParams()
+    const { id } = useParams()
+    const query = new URLSearchParams(useLocation().search)
 
     return (
-        // <h2>Id is = {match.params.id} </h2>
-        <h2>Id is = {id}</h2>
+        <>
+        {/* <h2>Id is = {match.params.id} </h2> */}
+            <h2>Id is = {id}</h2>
+            <h2>{query.get('first')}</h2>
+            <h2>{query.get('last')}</h2>
+        </>
     );
 };
 

4a6c1e0fb1b46c93dd011da69cece7602fceef7b
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-11-01 13:42:58 +0300

    nested routing

---
+++ src/components/ViewProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewProfile = () => {
+    return (
+        <>
+          <h2>ViewProfile Page</h2>      
+        </>
+    );
+};
+
+export default ViewProfile;
